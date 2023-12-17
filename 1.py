e42fa1a33574d7488e0bd2b22824fb66329ee85a
Sikander-Ali-Bhatti <152159856+Sikander-Ali-Bhatti@users.noreply.github.com>
2023-12-17 08:25:55 +0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,11 @@
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+print(F"a*a") (b*b)
+a
+b
+if a%2==1  and b%2==1:
+    result=(a*a)+(b*b)
+    print(result)
+else:
+    print("calculation is not performed".)
+    
